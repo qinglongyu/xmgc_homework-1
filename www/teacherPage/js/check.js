291a80503abfc37e23f7e6da823dc01d5cd52a70
love1438 <729260406@qq.com>
2016-09-21 13:58:34 +0800

    update test

--- www/teacherPage/js/check.js
+++ www/teacherPage/js/check.js
@@ -1,13 +1,14 @@
 var userid;
 var nick;
-$.post('/../../start/api/getMyInfo',function (res) {
-		console.log('>>>',res.data);
-		if(res.text=='没找到您的登录信息，请重新登陆或注册.'){
-			alert("没找到您的登录信息，请重新登陆或注册.");
-		window.location.href='http://m.xmgc360.com/start/web/account/'
-		}
-		userid=res.data['id'];
-		nick=res.data['nick'];
-//		console.log(">>>userid:",userid);
-//		console.log(">>>nick:",nick);
-	})
+$.post('/../../start/api/getMyInfo', function (res) {
+	console.log('>>>', res.data);
+	if (res.text == '没找到您的登录信息，请重新登陆或注册.') {
+		alert("没找到您的登录信息，请重新登陆或注册.");
+		window.location.href = 'http://m.xmgc360.com/start/web/account/'
+	}
+	userid = res.data['id'];
+	nick = res.data['nick'];
+	//		console.log(">>>userid:",userid);
+	//		console.log(">>>nick:",nick);
+
+})
